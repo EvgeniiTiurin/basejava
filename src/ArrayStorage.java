61537b7f81dda4ecb10fb3de4f2fec576cb43659
Eugene <sirius-tyrin@yandex.ru>
2021-03-10 16:26:08 +0300

    list - size + save + delete - get + (Но не отрабатывает неизвестный вариант clear +

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,73 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[5];
+    int index = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        index = 0;
     }
 
     void save(Resume r) {
+        if (r.uuid != null) {
+            storage[index] = r;
+            index++;
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume r : storage) {
+            if (r.uuid == uuid) {
+                return r;
+            }
+        }
+        System.out.println("Не найдено");
         return null;
     }
 
     void delete(String uuid) {
+        for (Resume r : storage) {
+            if (r.uuid == uuid) {
+                System.arraycopy(storage, Arrays.asList(storage).indexOf(r) + 1, storage, Arrays.asList(storage).indexOf(r), storage.length - 1 - Arrays.asList(storage).indexOf(r));
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) ;
+            count++;
+        }
+
+        Resume[] temp = new Resume[5]; //storage.length - count];
+        int j = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                temp[j] = storage[i];
+                j++;
+            }
+        }
+        storage = temp;
+        return storage;
+        //return new Resume[0];
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+            count++;
+            }
+        }
+        return count;
     }
 }
