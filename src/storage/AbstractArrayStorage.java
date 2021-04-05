f688e539427890b35b8ee37c163f09f2d572c7ba
Eugene <sirius-tyrin@yandex.ru>
2021-04-05 14:35:54 +0300

    HW3 new

--- src/storage/AbstractArrayStorage.java
+++ src/storage/AbstractArrayStorage.java
@@ -2,6 +2,8 @@ package storage;
 
 import model.Resume;
 
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -25,4 +27,53 @@ public abstract class AbstractArrayStorage implements Storage {
     }
 
     protected abstract int getIndex(String uuid);
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            storage[index] = resume;
+            System.out.println("Резюме " + resume + " успешно обновлено");
+            return;
+        }
+        System.out.println("ERROR: Резюме " + resume.getUuid() + " для обновления не найдено");
+    }
+
+    public void save(Resume resume) {
+        if (resumeCounter < ARRAY_SIZE) {
+            if (resume.getUuid() != null) {
+                if (getIndex(resume.getUuid()) >= 0) {
+                    System.out.println("ERROR: Резюме " + resume + " уже внесено в базу");
+                    return;
+                }
+                storage[resumeCounter] = resume;
+                resumeCounter++;
+                System.out.println("Резюме " + resume + " добавлено в базу");
+            } else {
+                System.out.println("ERROR: Вы ввели пустое значение");
+            }
+        } else {
+            System.out.println("ERROR: Достигнут максимум массива резюме");
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("ERROR: Резюме " + uuid + " не найдено");
+            return;
+        }
+        System.out.println("Резюме " + storage[index].getUuid() + " успешно удалено из базы");
+        System.arraycopy(storage, index + 1, storage, index, resumeCounter - 1 - index);
+        resumeCounter--;
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, resumeCounter);
+    }
+    
+    public void clear() {
+        Arrays.fill(storage, 0, resumeCounter, null);
+        resumeCounter = 0;
+        System.out.println("Массив успешно очищен");
+    }
 }
--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -1,15 +1,7 @@
 package storage;
 
-import model.Resume;
-
-import java.util.Arrays;
-
-/**
- * Array based storage for Resumes
- */
-
 public class ArrayStorage extends AbstractArrayStorage {
-
+    @Override
     protected int getIndex(String uuid) {
         for (int i = 0; i < resumeCounter; i++) {
             if (uuid.equals(storage[i].getUuid())) {
@@ -17,57 +9,5 @@ public class ArrayStorage extends AbstractArrayStorage {
             }
         }
         return -1;
-    }
-
-    public void clear() {
-        Arrays.fill(storage, 0, resumeCounter, null);
-        resumeCounter = 0;
-        System.out.println("Массив успешно очищен");
-    }
-
-    public void update(Resume resume) {
-        int index = getIndex(resume.getUuid());
-        if (index >= 0) {
-            storage[index] = resume;
-            System.out.println("Резюме " + resume + " успешно обновлено");
-            return;
-        }
-        System.out.println("ERROR: Резюме " + resume.getUuid() + " для обновления не найдено");
-    }
-
-    public void save(Resume resume) {
-        if (resumeCounter < ARRAY_SIZE) {
-            if (resume.getUuid() != null) {
-                if (getIndex(resume.getUuid()) >= 0) {
-                    System.out.println("ERROR: Резюме " + resume + " уже внесено в базу");
-                    return;
-                }
-                storage[resumeCounter] = resume;
-                resumeCounter++;
-                System.out.println("Резюме " + resume + " добавлено в базу");
-            } else {
-                System.out.println("ERROR: Вы ввели пустое значение");
-            }
-        } else {
-            System.out.println("ERROR: Достигнут максимум массива резюме");
-        }
-    }
-
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("ERROR: Резюме " + uuid + " не найдено");
-            return;
-        }
-        System.out.println("Резюме " + storage[index].getUuid() + " успешно удалено из базы");
-        System.arraycopy(storage, index + 1, storage, index, resumeCounter - 1 - index);
-        resumeCounter--;
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, resumeCounter);
     }
 }
