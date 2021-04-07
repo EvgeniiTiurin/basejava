1f1fcbd57c2a840512b89067aa768af775d38305
Eugene <sirius-tyrin@yandex.ru>
2021-04-07 15:02:23 +0300

    HW3 new

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -10,10 +10,13 @@ public class MainTestArrayStorage {
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
-        r1.setUuid("uuid1");
+        //r1.setUuid("uuid1");
         Resume r2 = new Resume();
-        r2.setUuid("uuid2");
+        //r2.setUuid("uuid2");
         Resume r3 = new Resume();
+        //r3.setUuid("uuid3");
+        r2.setUuid("uuid2");
+        r1.setUuid("uuid1");
         r3.setUuid("uuid3");
 
         ARRAY_STORAGE.save(r1);
--- src/storage/SortedArrayStorage.java
+++ src/storage/SortedArrayStorage.java
@@ -11,4 +11,25 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, resumeCounter, searchKey);
     }
+
+    @Override
+    public void save(Resume resume) {
+        //super.save(resume);
+        if (resumeCounter < ARRAY_SIZE) {
+            if (resume.getUuid() != null) {
+                if (getIndex(resume.getUuid()) >= 0) {
+                    System.out.println("ERROR: Резюме " + resume + " уже внесено в базу");
+                    return;
+                }
+                storage[resumeCounter] = resume;
+                Arrays.sort(storage);
+                resumeCounter++;
+                System.out.println("Резюме " + resume + " добавлено в базу");
+            } else {
+                System.out.println("ERROR: Вы ввели пустое значение");
+            }
+        } else {
+            System.out.println("ERROR: Достигнут максимум массива резюме");
+        }
+    }
 }
