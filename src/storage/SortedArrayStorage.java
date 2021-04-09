4fa98eecf460b973a236c53a72c333f42e0ea711
Eugene <sirius-tyrin@yandex.ru>
2021-04-09 16:41:12 +0300

    update

--- src/storage/SortedArrayStorage.java
+++ src/storage/SortedArrayStorage.java
@@ -15,15 +15,10 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void adoptedSave(Resume resume) {
-        if (resumeCounter > 0) {
             int index = Arrays.binarySearch(storage, 0, resumeCounter, resume, Comparator.nullsLast((Object o1, Object o2) -> ((Resume) o1).compareTo((Resume) o2)));
-            if (index < 0) {
-                index = -index - 1;
-            }
+            index = -index - 1;
+
             System.arraycopy(storage, index, storage, index + 1, resumeCounter - index);
             storage[index] = resume;
-        } else {
-            storage[resumeCounter] = resume;
-        }
     }
 }
